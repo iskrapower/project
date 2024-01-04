be17ceeb00f155ffe62eb2877f5293c532645da8
Ilkham <ilkham200627@gmail.com>
2024-01-04 15:19:03 +0600

    Giving a reply message from site

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
+import json
 
 bot = Bot('6859939400:AAFeizQ3YrvecO5BAK8MIpHWAantRY8yxo0')
 dp = Dispatcher(bot)
@@ -12,6 +13,11 @@ async def start(message):
         url='https://iskrapower.github.io/project')))
     await message.answer('Привет, мой друг!', reply_markup=markup)
 
+
+@dp.message_handler(content_types=['web_app_data'])
+async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f'Name: {res["name"]}. Email: {res["email"]}. Phone: {res["phone"]}')
 
 executor.start_polling(dp)
 
